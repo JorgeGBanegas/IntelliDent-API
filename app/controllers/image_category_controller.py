cc1d914d8ed6682688ab9087fc4f61abe4929859
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-06-05 23:01:59 -0400

    feat: Modificar ruta y controlador de categoria de imagenes
    -Se modifico la ruta para agragar la proteccion de cognito
    -Se modifico el controlador para capturar excepciones Http

--- app/controllers/image_category_controller.py
+++ app/controllers/image_category_controller.py
@@ -1,4 +1,5 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from starlette import status
 
 from app.dependencies.dependencies import get_image_category_service
 from app.services.image_category_service import ImageCategoryService
@@ -12,4 +13,7 @@ class ImageCategoryController:
         try:
             return self.image_category_service.get_all_categories()
         except Exception as e:
-            raise ValueError(f"Error al obtener categorías: {e}")
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail={
+                "message": "Error al obtener categorias",
+                "error": str(e)
+            })
